47b1aeccad3eb5efc6a368681815a062308fa851
arindalC <arindal.contreras@itam.mx>
2025-12-10 15:09:39 -0600

    Frontend: landing responsive, navbar funcional, login y registro

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { UserProvider } from './context/UserContext.jsx'
+import './index.css'
+import { BrowserRouter } from "react-router-dom"; // Importamos BrowserRouter que nos permite usar rutas para navegar entre paginas
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <UserProvider> 
-      <App />
-    </UserProvider>
-  </StrictMode>,
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
